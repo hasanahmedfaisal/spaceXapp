41e28b71f172deca4aa543438222b078171c46b0
hasana <hasan.ahmed@thetrainline.com>
2020-08-27 11:13:26 +0100

    fixed heroku build error

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.prepare()
         handle(req, res, parsedUrl)
       }
     })
-      .listen(3000, () => {
-        console.log(`> Ready on http://localhost:${3000}`)
+      .listen(process.env.PORT || 3000, () => {
+        console.log(`> Ready on http://localhost:${process.env.PORT || 3000}`)
       })
   })
