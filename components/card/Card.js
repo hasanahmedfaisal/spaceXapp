83f965c4948f908348e5ddd32bd2b3d195de2041
hasanahmedfaisal <43376627+hasanahmedfaisal@users.noreply.github.com>
2020-08-28 11:48:53 +0100

    removed custom function (#4)

--- components/card/Card.js
+++ components/card/Card.js
@@ -2,27 +2,9 @@ import React from 'react'
 import isEmpty from 'lodash/isEmpty'
 
 export default class Card extends React.Component {
-  canUseWebP () {
-    var elem = document.createElement('canvas')
-
-    if (elem.getContext && elem.getContext('2d')) {
-      // was able or not to get WebP representation
-      return elem.toDataURL('image/webp').indexOf('data:image/webp') === 0
-    }
-
-    // very old browser like IE 8, canvas not supported
-    return false
-  }
-
   fetchImageSrc (missionPatchImage) {
     try {
-      if (missionPatchImage == null) {
-        return ''
-      } else if (this.canUseWebP()) {
-        return `/${missionPatchImage.split('/')[5].split('.')[0]}.webp`
-      } else {
-        return missionPatchImage
-      }
+      if (missionPatchImage == null) { return '' } else { return `/${missionPatchImage.split('/')[5].split('.')[0]}.webp` }
     } catch (error) {
       return missionPatchImage
     }
@@ -40,7 +22,10 @@ export default class Card extends React.Component {
     const imageSrc = this.fetchImageSrc(missionPatchImage)
     return (
       <div className='mainCard'>
-        <img alt='Image Unavailable' src={imageSrc} />
+        <picture>
+          <source srcSet={imageSrc} type='image/webp' />
+          <img alt='Image Unavailable' src={missionPatchImage} />
+        </picture>
         <div className='text missionName'>{missionName} #{flightNumber}</div>
         <div className='text missionId'>Mission Ids:
           <div className='missionIds'>{isMissionIdPresent ? missionId.join() : 'Unavailable'}</div>
